d8ef9989bfb6d65812a02f787b101cd45d0fcda8
MarcusSkalevik <marcusandreasskaal@gmail.com>
2022-08-03 12:24:37 +0200

    Update

--- project/nodeapp/public/login-page.js
+++ project/nodeapp/public/login-page.js
@@ -4,10 +4,9 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "ilder") {
+    if ( password === "ilder") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
