252ac622533b3ac7a098631ec61d72888ca66965
MarcusSkalevik <marcusandreasskaal@gmail.com>
2022-08-08 14:38:45 +0200

    update

--- project/nodeapp/public/login-page.js
+++ project/nodeapp/public/login-page.js
@@ -1,16 +1,15 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const hash = await argon2.hash(password, {type: argon2.argon2id});
-z
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    loginForm.password.value;
-   
+    const password = loginForm.password.value;
 
-
-   
+    if (password === "ilder") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
 })
-
-
