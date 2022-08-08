dedc7732aeac7340bff781572cf9c6770260c7a2
yrno <henriksfurdal@outlook.com>
2022-08-08 15:55:23 +0200

    integrate argon2 to a secondary login-page

--- project/nodeapp/public/login-page.js
+++ project/nodeapp/public/login-page.js
@@ -1,15 +1,27 @@
+const { argon2d } = require(“argon2”);
+
 const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
+//const loginButton = document.getElementById("login-form-submit");
+const hashedLoginButton = document.getElementById("hashed-login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
+const hash = '$argon2id$v=19$m=4096,t=3,p=1$rJhrZxCuDVncbNVRzC6rOw$uO4Ww0NZCdwABYtNKjW4ZEHAVlyzMLmDhGXqXwaKfMA'
+// loginButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const password = loginForm.password.value;
+
+//     if (password === "ilder") {
+//         alert("You have successfully logged in.");
+//         location.reload();
+//     } else {
+//         loginErrorMsg.style.opacity = 1;
+//     }
+// })
+
+hashedLoginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const password = loginForm.password.value;
-
-    if (password === "ilder") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
+    console.log("HELLO I AM HERE I AM WORKING HELLOOOOOOOOOOO!!!!")
+    const correct = argon2d.verify(hash, password)
+    console.log(correct ? "Matches" : "Do not match")
 })
