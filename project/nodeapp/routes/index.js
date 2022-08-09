be47236e888c6cc56901cfaaa0858dfed19e4336
MarcusSkalevik <marcusandreasskaal@gmail.com>
2022-08-09 12:37:50 +0200

    created new routing to hash?

--- project/nodeapp/routes/index.js
+++ project/nodeapp/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const { request, response } = require('../app');
 var router = express.Router();
+var login = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+login.post('/api', (request, response) => {
+  console.log(request);
+})
+
 
 module.exports = router;
