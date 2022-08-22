76a1b60daaa278274ee2dbe1e1db6594da8a5383
yrno <henriksfurdal@outlook.com>
2022-08-22 14:06:52 +0200

    add hashing in backend with http request

--- project/nodeapp/routes/hashedlogin.js
+++ project/nodeapp/routes/hashedlogin.js
@@ -1,8 +1,25 @@
+const argon2 = require("argon2")
 var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('hashedlogin');
+    res.render('hashedlogin')
+});
+
+router.post('/', async (req, res) => {
+    const userinput = req.body.password;
+
+    const hash = await argon2.hash(userinput, {type: argon2.argon2id});
+    console.log(hash);
+    const correct = await argon2.verify(hash, "ilder");
+    console.log(correct);
+
+    if (correct) {
+        res.render('welcome')
+        return;
+    }
+
+    res.render('hashedlogin')
 });
 
 module.exports = router;
